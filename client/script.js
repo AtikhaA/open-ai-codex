8d3da94fbbbff7ba781a9f8acd67ece921f04d77
AtikhaA <atikhazmila02@gmail.com>
2022-12-26 14:33:22 +0700

    third

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response 
 
-  const response = await fetch('https://open-ai-codex-6oxa4ap48-atikhaa.vercel.app/', {
+  const response = await fetch('https://open-ai-codex-omega.vercel.app/', {
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json'
